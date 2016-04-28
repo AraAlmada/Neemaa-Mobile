e8f1ecfd152434791e6e4e933781983392188deb
Benjamin Sansy <benjamin.sansy@gmail.com>
2016-04-28 14:18:16 +0200

    controle des champs du formulaire inscription

--- www/app/register/RegisterController.js
+++ www/app/register/RegisterController.js
@@ -1,5 +1,19 @@
-appContext.controller('RegisterController',function($scope){
-    $scope.register = function (form) {
-        
+appContext.controller('RegisterController',function($scope, ionicToast){
+    $scope.register = function (req) {
+      if (req.mail == undefined) {
+        ionicToast.show('Remplir le champs Email', 'top', true, 2500);
+      }
+      if (req.password == undefined) {
+        ionicToast.show('Mot de passe non défini', 'top', true, 2500);
+      }
+      if (req.password_confirm == undefined) {
+        ionicToast.show('Il vous fai confirmer votre mot de passe', 'top', true, 2500);
+      }
+      if (req.password.length < 6) {
+        ionicToast.show('Votre mot de passe doit contenir au moins 6 charactères', 'top', true, 2500);
+      }
+      if (req.password != req.password_confirm) {
+        ionicToast.show('Les mot de passe ne corresponde pas !', 'top', true, 2500);
+      }
     }
 });
