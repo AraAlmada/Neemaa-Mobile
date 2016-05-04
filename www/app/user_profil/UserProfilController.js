b232dc640f7edf446165fc9f56a5d40f03f9adbd
Benjamin Sansy <benjamin.sansy@gmail.com>
2016-05-04 12:02:43 +0200

    fix bug request update user

--- www/app/user_profil/UserProfilController.js
+++ www/app/user_profil/UserProfilController.js
@@ -1,6 +1,20 @@
-appContext.controller('UserProfilController',function($scope, UserProfilService){
-  $scope.user = UserProfilService.getUser;
-  $scope.user_profil = function (req) {
-    console.log(req);
+appContext.controller('UserProfilController',function($scope, UserProfilService, localStorageService){
+  $scope.user_profil = [];
+  UserProfilService.getUser(localStorageService.get('token'))
+    .success(function (data) {
+      $scope.user_profil = JSON.parse(data.data);
+  })
+    .error(function (err) {
+    console.log(err);
+  });
+  $scope.save_profil = function (req) {
+    alert(req.birthdate);
+    UserProfilService.saveUser(req, localStorageService.get('token'))
+      .success(function (data) {
+        console.log(date);
+    })
+      .error(function (err) {
+        console.log(err);
+    });
   }
 });
--- www/app/user_profil/UserProfilService.js
+++ www/app/user_profil/UserProfilService.js
@@ -1,50 +1,43 @@
 appContext.factory('UserProfilService',function($http){
   return {
-    getUser: {
-      'firstname': 'Dupont',
-      'lastname': 'Jean',
-      'email': 'jean.dupont@gmail.com',
-      'tel': '0699129912',
-      'adress': '7 avenue des arts',
-      'cp': '94100',
-      'birsthdate': '14/11/1990',
-      'SkinType': [
-        'Normal',
-        'Sèche',
-        'Mixte',
-        'Grasse',
-        'Grasse à tendance acnéique',
-        'Hyper pigmentée'
-      ],
-      'SkinColor': [
-        '#fbd3d1',
-        '#f2b8ad',
-        '#edc192',
-        '#d9ae7b',
-        '#C0946F',
-        '#9C6C44',
-        '#7F553D',
-        '#674230',
-        '#4A2F24',
-        '#3C1F0F',
-        '#2E2520'
-      ],
-      'HairType': [
-        'Cheveux raide',
-        'Ondulé',
-        'Bouclés-frisé',
-        'Crépus'
-      ],
-      'HairColor': [
-        'Noir',
-        'Châtain foncée',
-        'Châtain',
-        'Châtain clair',
-        'Blond foncée',
-        'Blond clair',
-        'Blond trés clair',
-        'Roux'
-      ]
+    getUser: function (value) {
+      return $http({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/user/get/profile?token=' + value,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+        },
+        data: {token: value}
+      });
+    },
+    saveUser: function (user, token) {
+      return $http({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/user/update/profile?token=' + token,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+        },
+        data: {
+          first_name: user.firstname,
+          last_name: user.lastname,
+          sex: user.gender,
+          code_postal: user.cp,
+          birthdate: user.birthdate,
+          skin_type: user.SkinType,
+          skin_color: user.SkinColor,
+          hair_type: user.HairType,
+          hair_color: user.HairColor,
+          token: token
+        }
+      });
     }
   }
 });
