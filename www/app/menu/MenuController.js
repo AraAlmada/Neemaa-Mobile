40f8cc02af30b1c286baa5d43f1ce5889764c1e0
Benjamin Sansy <benjamin.sansy@gmail.com>
2016-05-06 10:45:21 +0200

    add menu ionicSideMenuDelegate to false

--- www/app/menu/MenuController.js
+++ www/app/menu/MenuController.js
@@ -1,9 +1,16 @@
-appContext.controller('MenuController', function($scope, $location){
-    $scope.showMenuButton = function () {
-      if ($location.path() == '/app/register' || $location.path() == '/app/login') {
-        return false;
-      } else {
-        return true;
-      }
-    };
+appContext.controller('MenuController', function($scope, $location, $ionicSideMenuDelegate){
+  $scope.showMenuButton = function () {
+    if ($location.path() == '/app/register' || $location.path() == '/app/login') {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  $scope.$on('$ionicView.enter', function(){
+    $ionicSideMenuDelegate.canDragContent(false);
+  });
+  $scope.$on('$ionicView.leave', function(){
+    $ionicSideMenuDelegate.canDragContent(false);
+  });
 });
