d648688797740d6dfce772f8c6f0f2fd7c59fca9
Benjamin Sansy <benjamin.sansy@gmail.com>
2016-05-06 14:43:00 +0200

    integration du menu profil selon l utilisateur

--- www/app/menu/MenuController.js
+++ www/app/menu/MenuController.js
@@ -1,9 +1,24 @@
-appContext.controller('MenuController', function($scope, $location, $ionicSideMenuDelegate){
+appContext.controller('MenuController', function($scope, $location, $ionicSideMenuDelegate, MenuService, localStorageService){
   $scope.showMenuButton = function () {
     if ($location.path() == '/app/register' || $location.path() == '/app/login') {
       return false;
     } else {
       return true;
+    }
+  };
+
+  $scope.isPartner = [];
+  MenuService.checkIsPartner(localStorageService.get('token'))
+  .success(function (data) {
+    $scope.isPartner = JSON.parse(data.data);
+    $scope.isPartner = $scope.isPartner.partner;
+  });
+
+  $scope.checkIsPartner = function () {
+    if ($scope.isPartner) {
+      return 1;
+    } else {
+      return 0;
     }
   };
 
