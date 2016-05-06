09596e73ca011aeed8299a8aff9cab8e0e76456a
Benjamin Sansy <benjamin.sansy@gmail.com>
2016-05-06 12:04:33 +0200

    add NFO to partner page fro current user

--- www/app/partner_profil/PartnerProfilService.js
+++ www/app/partner_profil/PartnerProfilService.js
@@ -1,7 +1,18 @@
 appContext.factory('PartnerProfilService', function ($http) {
   return {
-    getNfo: {
-      
+    getNfo: function (value) {
+      return $http({
+        method: 'POST',
+        url: 'http://52.33.106.148/Neemaa-Back/web/app.php/api/user/get/profile?token=' + value,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+        },
+        data: {token: value}
+      });
     }
   }
 });
